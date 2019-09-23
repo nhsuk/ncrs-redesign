503d8c7feb0b7b269d1b259fd93bd269d13d5d52
Dan Johnston <daniel.johnston1@nhs.net>
2019-09-23 16:11:30 +0100

    proto changes

--- app/assets/javascript/modal.js
+++ app/assets/javascript/modal.js
@@ -16,6 +16,16 @@ $( ".emergency-btn" ).click(function() {
 
 });
 
+$('#submit-ptv').click(function(e){
+   if($('#more-detail').val() < 4 || $('#more-detail').val() > 50){
+      $('#ptv-char-length-error').removeClass('hidden');
+      $('#ptv-char-forbidden-error').removeClass('hidden');
+      $('#explanation-form-group').addClass('nhsuk-form-group--error');
+      e.preventDefault();
+   }
+});
+
+
 $(document).click(function(event) {
   //if you click on anything except the modal itself or the "open modal" link, close the modal
   if (!$(event.target).closest(".modal-content,.open_modal").length) {
