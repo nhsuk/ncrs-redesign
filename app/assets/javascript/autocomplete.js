71958656e2bb3ee67658d9f60459bade1c4d05d6
Dan Johnston <daniel.johnston1@nhs.net>
2019-11-25 15:03:39 +0000

    RA Flag updates

--- app/assets/javascript/autocomplete.js
+++ app/assets/javascript/autocomplete.js
@@ -1,6 +1,5 @@
 $(window).resize(function() {
   var inputWidth = $("#sites").width();
-  console.log(inputWidth);
 });
 
 $(function() {
@@ -569,22 +568,32 @@ $(function() {
     $("#ra-flags").val("");
   });
 
+  var adjustmentsList = [];
 
   $('#add-ra-flag-btn').click(function(e) {
     var flagVal = $(".modal-flag-desc").text();
     var catVal = $(".modal-flag-category").text();
-    console.log(flagVal);
+    var infoVal = $("#ra-supporting-info").val();
     var numFlags = $(".added-ra-flag").length;
+    if (infoVal == "") {
+      var infoVal = "No supporting information added"
+    } else {
+      var infoVal = $("#ra-supporting-info").val();
+    }
     if (flagVal != "" && numFlags > 0) {
-      $(".added-adjustments-list").append("<div class='added-ra-flag added-ra-flag-border-top'><span class='flag-category'>" + catVal + "</span><span class='flag-desc'>" + flagVal + "</span><span class='remove-flag-container'><a href='#' class='remove-flag-link'>Remove</a></span></div>");
+      $(".added-adjustments-list").append("<div class='added-ra-flag added-ra-flag-border-top'><span class='flag-category'>" + catVal + "</span><span class='flag-desc'>" + flagVal + "</span><span class='ra-support-info'>" + infoVal + "</span><span class='remove-flag-container'><a href='#' class='remove-flag-link'>Remove</a></span></div>");
       $("#ra-flags").val("");
       var numFlags = $(".added-ra-flag").length;
     } else if (flagVal != "") {
       $('.no-adjustments-placeholder').hide();
-      $(".added-adjustments-list").append("<div class='added-ra-flag'><span class='flag-category'>" + catVal + "</span><span class='flag-desc'>" + flagVal + "</span><span class='remove-flag-container'><a href='#' class='remove-flag-link'>Remove</a></span></div>");
+      $(".added-adjustments-list").append("<div class='added-ra-flag'><span class='flag-category'>" + catVal + "</span><span class='flag-desc'>" + flagVal + "</span><span class='ra-support-info'>" + infoVal + "</span><span class='remove-flag-container'><a href='#' class='remove-flag-link'>Remove</a></span></div>");
       $("#ra-flags").val("");
     }
+    $("#ra-supporting-info").val("");
+    adjustmentsList.push(catVal + " | " + flagVal + " | " + infoVal);
+    sessionStorage.setItem("adjustmentsList", JSON.stringify(adjustmentsList));
   });
+
 
   $(document).on('click', '.remove-flag-link', function() {
     var numFlags = $(".added-ra-flag").length;
@@ -648,6 +657,17 @@ $(function() {
     $('.adjustments-floater').html("Adjustments added: " + numFlags);
   });
 
-
-
 });
+
+if (window.location.pathname == "/ra-flag_v2/ra-step-4") {
+  var adjustmentsList = JSON.parse(sessionStorage.getItem("adjustmentsList"));
+  var arrayLength = adjustmentsList.length;
+  console.log(arrayLength);
+  if (arrayLength >= 1) {
+    $(".no-adjustments-added-text").hide();
+  }
+  for (i = 0; i < arrayLength; i++) {
+    var adjustmentArray = adjustmentsList[i].split('|');
+    $('<div class="scra-card-content"><div class="nhsuk-grid-row"><div class="nhsuk-grid-column-three-quarters nhsuk-u-three-quarters-tablet"><div class="card-content-section"><h4 class="nhsuk-heading-s"><span class="nhsuk-caption-s nhsuk-caption--bottom">' + adjustmentArray[0] + '</span><span role="text">' + adjustmentArray[1] + '</span></h4><h5 class="nhsuk-heading-xs"><span role="text">Supporting information:</span><span class="nhsuk-caption-m nhsuk-caption--bottom">' + adjustmentArray[2] + '</span></h5><p class="nhsuk-body-s">Added: 26-Apr-2019 by James Smith (GO) at Leeds Teaching Hospitals NHS Trust</p></div></div><div class="nhsuk-grid-column-one-quarter nhsuk-u-one-quarter-tablet"><a href="#" class="nhsuk-button nhsuk-button--blue ra-edit">Edit</a><a href="#" class="nhsuk-button nhsuk-button--red ra-delete">Delete</a></div></div></div>').appendTo($("#added-adjustments-summary"));
+  }
+}
