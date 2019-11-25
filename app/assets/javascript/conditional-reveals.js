1dfcf4a499eb8c21dc1e341021969e6083d928f5
Dan Johnston <daniel.johnston1@nhs.net>
2019-11-25 16:24:05 +0000

    added validation to ra step 1

--- app/assets/javascript/conditional-reveals.js
+++ app/assets/javascript/conditional-reveals.js
@@ -21,6 +21,25 @@ $(document).ready(function() {
     }
   });
 
+});
 
+$("#ra-step-1-form").submit(function(e) {
+  if ($('#consent-best-interest').is(':checked') && !$('#bi-consent').val().length > 0) {
+    $('.nhsuk-error-summary__list').empty();
+    $('.nhsuk-error-summary').show();
+    $('#best-interest-consent-error').show();
+    $('.nhsuk-error-summary__list').append('<li><a href="#best-interest-consent-error">You must enter more details about a best interest consent decision</a></li>');
+    $('#best-interest').css('border-left', '4px solid #d5281b');
+    e.preventDefault();
+  }
+
+  if ($('#consent-other').is(':checked') && !$('#other-consent-input').val().length > 0) {
+    $('.nhsuk-error-summary__list').empty();
+    $('.nhsuk-error-summary').show();
+    $('#other-consent-error').show();
+    $('.nhsuk-error-summary__list').append('<li><a href="#other-consent-error">You must enter more details about a consent decision obtained by other methods</a></li>');
+    $('#other-consent').css('border-left', '4px solid #d5281b');
+    e.preventDefault();
+  }
 
 });
