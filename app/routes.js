6183238808ae76b194dd489a11caf349495d1639
Nick Colley <nick.colley@hippodigital.co.uk>
2023-02-22 11:30:01 +0000

    Remove conditioanl reveals javascript

--- app/routes.js
+++ app/routes.js
@@ -39,9 +39,52 @@ router.post("/locum-pharmacist/confirm-site", function (req, res) {
   if (locumSite) {
     res.redirect("/locum-pharmacist/confirm-role");
   } else {
+    req.session.data.errors = {};
     req.session.data.errors.locumSite = true;
     res.redirect("/locum-pharmacist/confirm-site");
   }
+});
+
+/**
+ * Reasonable Adjustment Flags
+ * */
+router.get("/ra-flag_v5/ra-step-1", function (req, res, next) {
+  // Reset any errors and copy it into the current template data
+  if (req.session.data.errors) {
+    res.locals.errors = req.session.data.errors;
+  }
+  req.session.data.errors = {};
+  next();
+});
+
+router.post("/ra-flag_v5/ra-step-1", function (req, res) {
+  const consent = req.body["consent"];
+  const bestInterestDetail = req.body["best-interest-consent-input"];
+  const otherConsentDetail = req.body["other-consent-input"];
+
+  req.session.data.errors = {};
+
+  // If there is no submitted option
+  if (!consent) {
+    req.session.data.errors["all"] = true;
+    return res.redirect("/ra-flag_v5/ra-step-1");
+  }
+  
+  if (consent === 'best-interest') {
+    if (!bestInterestDetail) {
+      req.session.data.errors["best-interest-consent-input"] = true;
+      return res.redirect("/ra-flag_v5/ra-step-1");
+    }
+  }
+  
+  if (consent === "other-consent") {
+    if (!otherConsentDetail) {
+      req.session.data.errors["other-consent-input"] = true;
+      return res.redirect("/ra-flag_v5/ra-step-1");
+    }
+  }
+  
+  res.redirect("/ra-flag_v5/ra-step-2");
 });
 
 module.exports = router;
