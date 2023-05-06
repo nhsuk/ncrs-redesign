8df90786a4e8ada235bd40020329242f2b521e29
Matt Fielding <matt@hippodigital.co.uk>
2023-05-06 15:17:49 +0100

    show data in console

--- app/routes.js
+++ app/routes.js
@@ -2,6 +2,21 @@
 const express = require("express");
 const router = express.Router();
 
+// show all data store items in the terminal window
+router.use((req, res, next) => {
+  const log = {
+    method: req.method,
+    url: req.originalUrl,
+    data: req.session.data,
+    patients: req.session.patients
+  }
+  // you can enable this in your .env file
+  if (process.env.LOGGING === 'TRUE') {
+    console.log(JSON.stringify(log, null, 2))
+  }
+  next()
+})
+
 require("./views/ra-flag/routes.js")(router);
 require("./views/search/routes.js")(router);
 
