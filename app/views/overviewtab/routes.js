fdd5e5854cf957e7effdf81cc95b66e47287eb6f
Matt Fielding <matt@hippodigital.co.uk>
2023-06-12 16:45:42 +0100

    search and scenario routing

--- app/views/overviewtab/routes.js
+++ app/views/overviewtab/routes.js
@@ -1,28 +1,3 @@
-//const moment = require("moment");
-//moment.locale("en-gb");
-
-//const patientList = require("../../data/patients-list.js");
-
-//function formatName(str) {
-//  return str
-//    .replace(/(\B)[^ ]*/g, (match) => match.toLowerCase())
-//    .replace(/^[^ ]/g, (match) => match.toUpperCase());
-//}
-
-//module.exports = (router) => {
-
-//router.get("/overviewtab/patient", function (req, res, next) {
-//let nhsNumber = req.session.data["nhs-number"];
-//let patientList = require("../../data/patients-list.js");
-//let patientsData = {};
-//patientsData = patientList[nhsNumber]
-//req.session.data['patient'] = patientsData;
-
-//return res.render('overviewtab/patient')
-// });
-
-//search function
-
 const moment = require("moment");
 moment.locale("en-gb");
 
@@ -37,6 +12,7 @@ function formatName(str) {
 module.exports = (router) => {
   router.post("/overviewtab/nhs-number-search", function (req, res) {
     const nhsNumber = req.body["nhs-number"];
+    const scenario = req.session.data["scenario"]
 
     req.session.data.errors = {};
 
@@ -49,165 +25,165 @@ module.exports = (router) => {
     res.redirect(`/overviewtab/search-results?nhs-number=${nhsNumber}`);
   });
 
-  // router.post("/search/basic-details-search", function (req, res) {
-  //   const gender = req.body["gender"];
-  //   const lastName = req.body["last-name"];
-  //   const dobDay = req.body["dob-day"];
-  //   const dobMonth = req.body["dob-month"];
-  //   const dobYear = req.body["dob-year"];
+  router.post("/overviewtab/basic-details-search", function (req, res) {
+    const gender = req.body["gender"];
+    const lastName = req.body["last-name"];
+    const dobDay = req.body["dob-day"];
+    const dobMonth = req.body["dob-month"];
+    const dobYear = req.body["dob-year"];
 
-  //   req.session.data.errors = {};
+    req.session.data.errors = {};
 
-  //  if (!gender) {
-  //    req.session.data.errors["gender"] = true;
-  //   }
-  //   if (!lastName) {
-  //     req.session.data.errors["last-name"] = true;
-  //  }
-  //   if (!dobDay || !dobMonth || !dobYear) {
-  //     req.session.data.errors["dob"] = true;
-  //   }
-  //
-  //   if (Object.keys(req.session.data.errors).length) {
-  //     return res.redirect("/search/basic-details-search");
-  //   }
+    if (!gender) {
+      req.session.data.errors["gender"] = true;
+    }
+    if (!lastName) {
+      req.session.data.errors["last-name"] = true;
+    }
+    if (!dobDay || !dobMonth || !dobYear) {
+      req.session.data.errors["dob"] = true;
+    }
 
-  // Format the data provided ready for processing
-  //   const date = new Date(dobYear, dobMonth - 1, dobDay);
-  //  const formattedDob = moment(date).format("DD-MMM-YYYY");
+    if (Object.keys(req.session.data.errors).length) {
+      return res.redirect("/overviewtab/basic-details-search");
+    }
 
-  //   const params = new URLSearchParams({
-  //     gender,
-  //    lastName: formatName(lastName),
-  //     dob: formattedDob,
-  //   });
-  //   res.redirect(`/overviewtab/search-results?${params}`);
-  //  });
+    // Format the data provided ready for processing
+    const date = new Date(dobYear, dobMonth - 1, dobDay);
+    const formattedDob = moment(date).format("DD-MMM-YYYY");
 
-  //  router.post("/overviewtab/advanced-details-search", function (req, res) {
-  //   const gender = req.body["gender"];
-  //   const firstName = req.body["first-name"];
-  //   const lastName = req.body["last-name"];
-  //   const dobDayFrom = req.body["dob-day-from"];
-  //   const dobMonthFrom = req.body["dob-month-from"];
-  //   const dobYearFrom = req.body["dob-year-from"];
-  //  const dobDayTo = req.body["dob-day-to"];
-  //   const dobMonthTo = req.body["dob-month-to"];
-  //   const dobYearTo = req.body["dob-year-to"];
-  //  const addressPostcode = req.body["address-postcode"];
-  //   const widenSearch = req.body["widen-search"];
+    const params = new URLSearchParams({
+      gender,
+      lastName: formatName(lastName),
+      dob: formattedDob,
+    });
+    res.redirect(`/overviewtab/search-results?${params}`);
+  });
 
-  //   req.session.data.errors = {};
+  router.post("/overviewtab/advanced-details-search", function (req, res) {
+    const gender = req.body["gender"];
+    const firstName = req.body["first-name"];
+    const lastName = req.body["last-name"];
+    const dobDayFrom = req.body["dob-day-from"];
+    const dobMonthFrom = req.body["dob-month-from"];
+    const dobYearFrom = req.body["dob-year-from"];
+    const dobDayTo = req.body["dob-day-to"];
+    const dobMonthTo = req.body["dob-month-to"];
+    const dobYearTo = req.body["dob-year-to"];
+    const addressPostcode = req.body["address-postcode"];
+    const widenSearch = req.body["widen-search"];
 
-  //   if (!gender) {
-  //req.session.data.errors["gender"] = true;
-  //   }
+    req.session.data.errors = {};
 
-  //    if (!firstName) {
-  //     req.session.data.errors["first-name"] = true;
-  //   }
+    if (!gender) {
+      req.session.data.errors["gender"] = true;
+    }
 
-  //   if (!lastName) {
-  //     req.session.data.errors["last-name"] = true;
-  //   }
+    if (!firstName) {
+      req.session.data.errors["first-name"] = true;
+    }
 
-  //   if (!dobDayTo || !dobMonthTo || !dobYearTo) {
-  //      req.session.data.errors["dob-to"] = true;
-  //    }
+    if (!lastName) {
+      req.session.data.errors["last-name"] = true;
+    }
 
-  //    if (!dobDayFrom || !dobMonthFrom || !dobYearFrom) {
-  //      req.session.data.errors["dob-from"] = true;
-  //    }
+    if (!dobDayTo || !dobMonthTo || !dobYearTo) {
+      req.session.data.errors["dob-to"] = true;
+    }
 
-  //    if (!addressPostcode) {
-  //     req.session.data.errors["address-postcode"] = true;
-  //    }
+    if (!dobDayFrom || !dobMonthFrom || !dobYearFrom) {
+      req.session.data.errors["dob-from"] = true;
+    }
 
-  //    if (Object.keys(req.session.data.errors).length) {
-  //      return res.redirect("/search/advanced-details-search");
-  //    }
+    if (!addressPostcode) {
+      req.session.data.errors["address-postcode"] = true;
+    }
 
-  //   const formattedPostcode = addressPostcode.toUpperCase().replace(/\s/g, "");
+    if (Object.keys(req.session.data.errors).length) {
+      return res.redirect("/overviewtab/advanced-details-search");
+    }
 
-  //   const fullDobFrom = dobDayFrom + "/" + dobMonthFrom + "/" + dobYearFrom;
-  //    const dobFrom = moment(fullDobFrom, "DD-MM-YYYY").format("MM/DD/YYYY");
+    const formattedPostcode = addressPostcode.toUpperCase().replace(/\s/g, "");
 
-  //   const fullDobTo = dobDayTo + "/" + dobMonthTo + "/" + dobYearTo;
-  //  const dobTo = moment(fullDobTo, "DD-MM-YYYY").format("MM/DD/YYYY");
+    const fullDobFrom = dobDayFrom + "/" + dobMonthFrom + "/" + dobYearFrom;
+    const dobFrom = moment(fullDobFrom, "DD-MM-YYYY").format("MM/DD/YYYY");
 
-  //   const params = new URLSearchParams({
-  //     gender,
-  //    firstName: formatName(firstName),
-  //     lastName: formatName(lastName),
-  //     postcode: formattedPostcode,
-  //     dobFrom,
-  //     dobTo,
-  //     widenSearch,
-  //   });
-  //  res.redirect(`/search/search-results?${params}`);
-  // });
+    const fullDobTo = dobDayTo + "/" + dobMonthTo + "/" + dobYearTo;
+    const dobTo = moment(fullDobTo, "DD-MM-YYYY").format("MM/DD/YYYY");
 
-  //  router.post("/search/postcode-search", function (req, res) {
-  //   const addressPostcode = req.body["postcode-only"];
-  //   req.session.data.errors = {};
+    const params = new URLSearchParams({
+      gender,
+      firstName: formatName(firstName),
+      lastName: formatName(lastName),
+      postcode: formattedPostcode,
+      dobFrom,
+      dobTo,
+      widenSearch,
+    });
+    res.redirect(`/overviewtab/search-results?${params}`);
+  });
 
-  //    if (!addressPostcode) {
-  //      req.session.data.errors["postcode-only"] = true;
-  //   }
+  router.post("/overviewtab/postcode-search", function (req, res) {
+    const addressPostcode = req.body["postcode-only"];
+    req.session.data.errors = {};
 
-  //   if (Object.keys(req.session.data.errors).length) {
-  //     return res.redirect("/search/postcode-search");
-  //   }
+    if (!addressPostcode) {
+      req.session.data.errors["postcode-only"] = true;
+    }
 
-  // Format the data provided ready for processing
-  //   const formattedPostcode = addressPostcode.toUpperCase().replace(/\s/g, "");
-  //   const params = new URLSearchParams({
-  //     postcode: formattedPostcode,
-  //   });
-  //   res.redirect(`/search/search-results?${params}`);
-  // });
+    if (Object.keys(req.session.data.errors).length) {
+      return res.redirect("/overviewtab/postcode-search");
+    }
+
+    // Format the data provided ready for processing
+    const formattedPostcode = addressPostcode.toUpperCase().replace(/\s/g, "");
+    const params = new URLSearchParams({
+      postcode: formattedPostcode,
+    });
+    res.redirect(`/overviewtab/search-results?${params}`);
+  });
 
   router.get("/overviewtab/search-results", function (req, res, next) {
     const nhsNumber = req.query["nhs-number"];
     const { gender, firstName, lastName, dob, dobTo, dobFrom, postcode } =
       req.query;
 
-    //   const isBasicSearch = gender && lastName && dob;
-    //   const isAdvancedSearch =
-    //      gender && firstName && lastName && dobTo && dobFrom && postcode;
-    //    const isPostcodeSearch = postcode && !isAdvancedSearch && !isBasicSearch;
-    //   const isNhsNumberSearch = nhsNumber && !isAdvancedSearch && !isBasicSearch;
+    const isBasicSearch = gender && lastName && dob;
+    const isAdvancedSearch =
+      gender && firstName && lastName && dobTo && dobFrom && postcode;
+    const isPostcodeSearch = postcode && !isAdvancedSearch && !isBasicSearch;
+    const isNhsNumberSearch = nhsNumber && !isAdvancedSearch && !isBasicSearch;
 
     const returnedPatientsList = {};
     for (const [patientNhsNumber, patient] of Object.entries(patientList)) {
       // Basic Details Search
-      //    if (
-      //       isBasicSearch &&
-      //       gender === patient.gender &&
-      //       lastName === patient.lastName &&
-      //       dob === patient.dob
-      //     ) {
-      //       returnedPatientsList[patientNhsNumber] = patient;
-      //       continue;
-      //    }
+      if (
+        isBasicSearch &&
+        gender === patient.gender &&
+        lastName === patient.lastName &&
+        dob === patient.dob
+      ) {
+        returnedPatientsList[patientNhsNumber] = patient;
+        continue;
+      }
       // Advanced Details Search
-      //    if (
-      //     isAdvancedSearch &&
-      //       (gender === patient.gender || gender === "Not known") &&
-      //       firstName === patient.firstName &&
-      //      lastName === patient.lastName &&
-      //      moment(new Date(dobFrom)).isBefore(new Date(patient.dob)) &&
-      //      moment(new Date(dobTo)).isAfter(new Date(patient.dob)) &&
-      //      postcode === patient.postcode
-      //    ) {
-      //      returnedPatientsList[patientNhsNumber] = patient;
-      //      continue;
-      //    }
+      if (
+        isAdvancedSearch &&
+        (gender === patient.gender || gender === "Not known") &&
+        firstName === patient.firstName &&
+        lastName === patient.lastName &&
+        moment(new Date(dobFrom)).isBefore(new Date(patient.dob)) &&
+        moment(new Date(dobTo)).isAfter(new Date(patient.dob)) &&
+        postcode === patient.postcode
+      ) {
+        returnedPatientsList[patientNhsNumber] = patient;
+        continue;
+      }
       // Postcode Details Search
-      //   if (isPostcodeSearch && postcode === patient.postcode) {
-      //     returnedPatientsList[patientNhsNumber] = patient;
-      //     continue;
-      //   }
+      if (isPostcodeSearch && postcode === patient.postcode) {
+        returnedPatientsList[patientNhsNumber] = patient;
+        continue;
+      }
       // NHS Number search
       if (isNhsNumberSearch && patientNhsNumber == nhsNumber) {
         returnedPatientsList[patientNhsNumber] = patient;
