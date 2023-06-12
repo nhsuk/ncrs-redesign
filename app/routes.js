04500209e744511aa90d9ca90c9d04aad96b0562
Jess <jessica.mancoo@hippodigital.co.uk>
2023-06-12 15:33:18 +0100

    routing

--- app/routes.js
+++ app/routes.js
@@ -8,39 +8,39 @@ router.use((req, res, next) => {
     method: req.method,
     url: req.originalUrl,
     data: req.session.data,
-    patients: req.session.patients
-  }
+    patients: req.session.patients,
+  };
   // you can enable this in your .env file
-  if (process.env.LOGGING === 'TRUE') {
-    console.log(JSON.stringify(log, null, 2))
+  if (process.env.LOGGING === "TRUE") {
+    console.log(JSON.stringify(log, null, 2));
   }
-  next()
-})
+  next();
+});
 
 require("./views/ra-flag/routes.js")(router);
 require("./views/search/routes.js")(router);
 require("./views/ovm/routes.js")(router);
-
+require("./views/overviewtab/routes.js")(router);
 
 // Dev Mode - Used to show routing by scenario other than user driven
 
 function devModeRoute(req, res, next) {
-  if (!req.session.data['devMode']) {
-    console.log('no data found')
-    var devMode = req.query.devMode
-    if (devMode === 'true') {
-      console.log('devmode detected')
-      req.session.data['devMode'] = 'true'
-      console.log('local storage updated')
+  if (!req.session.data["devMode"]) {
+    console.log("no data found");
+    var devMode = req.query.devMode;
+    if (devMode === "true") {
+      console.log("devmode detected");
+      req.session.data["devMode"] = "true";
+      console.log("local storage updated");
     } else {
-      console.log('devmode not detected')
+      console.log("devmode not detected");
     }
   } else {
-    console.log('data found and set to ' + req.session.data['devMode'])
+    console.log("data found and set to " + req.session.data["devMode"]);
   }
-  next()
+  next();
 }
 
-router.get('/*', devModeRoute)
+router.get("/*", devModeRoute);
 
 module.exports = router;
